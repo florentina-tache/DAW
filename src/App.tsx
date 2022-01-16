4cdd89b43ddf451a944a7bf80c1d04b01c85a54e
flori <florentina.tache8@gmail.com>
2022-01-16 14:42:08 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
